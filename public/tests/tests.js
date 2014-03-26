6254e0d7194a9cb660d7f8ebb51efe31b3009d2f
Cristo González Rodríguez <alu0100694987@ull.edu.es>
2014-03-26 22:13:50 +0000

    Corrigiendo estilo

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -2,7 +2,7 @@ var assert = chai.assert;
 
 suite('Tests', function(){
   
-  test('Asignaci&oacute;n', function(){
+  test('Asignacion: ', function(){
     obj = pl0.parse("x = 9 .")
     assert.equal(obj[0].type, "=")
     assert.equal(obj[0].left.type, "ID")
@@ -11,55 +11,55 @@ suite('Tests', function(){
     assert.equal(obj[0].right.value, "8") 
   });
 
-  test('Suma', function(){
+  test('Suma: ', function(){
     obj = pl0.parse("x = 3 + 5 .")
     assert.equal(obj[0].right.type, "+")
   });
 
-  test('Multiplicaci&oacute;n', function(){
+  test('Multiplicacion: ', function(){
     obj = pl0.parse("x = 4 * 1 .")
     assert.equal(obj[0].right.type, "*") 
   });
 
-  test('Divisi&oacute;n', function(){
+  test('Division: ', function(){
     obj = pl0.parse("x = 1 / 6 .")
     assert.equal(obj[0].right.type, "/")
   });
 
   
-  test('Asociatividad de izquierda', function(){
+  test('Asociatividad de izquierda: ', function(){
     obj = pl0.parse("x = 1-2-3 .")
     assert.equal(obj[0].right.left.type, "-") 
   });
   
-  test('Par&eacute;ntesis', function(){
+  test('Parentesis: ', function(){
     obj = pl0.parse("x = (2+4) * 9 .")
     assert.equal(obj[0].right.left.type, "+")
   });
   
-  test('Precedencia', function(){
+  test('Precedencia: ', function(){
     obj = pl0.parse("x = 2+3*3 .")
     assert.equal(obj[0].right.left.type, "NUM")
   });
 
-  test('Comparaci&oacute;n', function(){
+  test('Comparacion: ', function(){
     obj = pl0.parse("IF x == 1 THEN y = 3 .")
     assert.equal(obj[0].condition.type, "==")
   });
 
-  test('Bloque', function(){
+  test('Bloque: ', function(){
     obj = pl0.parse("CONST x = 3; VAR y; PROCEDURE p; x = x + 12; CALL p.")
     assert.equal(obj[0].left.type, "CONST ID")
     assert.equal(obj[1].type, "VAR ID")
     assert.equal(obj[2].type, "PROCEDURE")
   });
 
-  test('Call', function(){
+  test('Call: ', function(){
     obj = pl0.parse("CALL z .")
     assert.equal(obj[0].type, "CALL")
   });
 
-  test('If - If Else', function(){
+  test('If - If Else: ', function(){
     obj = pl0.parse("IF x == 2 THEN b = 2.")
     assert.equal(obj[0].type, "IF")
 
@@ -67,22 +67,22 @@ suite('Tests', function(){
     assert.equal(obj[0].type, "IFELSE")
   });
 
-  test('Odd', function(){
+  test('Odd: ', function(){
     obj = pl0.parse("IF ODD 3 THEN b = 23 .")
     assert.equal(obj[0].condition.type, "ODD")
   });
 
-  test('While Do', function(){
+  test('While Do: ', function(){
     obj = pl0.parse("WHILE x == 3 DO z = z+3.")
     assert.equal(obj[0].type, "WHILE")
   });
 
-  test('Begin End', function(){
+  test('Begin End: ', function(){
     obj = pl0.parse("BEGIN x = 3; z = b+3 END.")
     assert.equal(obj[0].type, "BEGIN")
   });
 
-  test('Argumentos de Call y Procedure', function(){
+  test('Argumentos de Call y Procedure: ', function(){
     obj = pl0.parse("VAR x, squ; PROCEDURE square(x,y,z); BEGIN squ = x * x END; CALL square(x).")
     assert.equal(obj[3].arguments[0].value, "x")
 
@@ -93,7 +93,7 @@ suite('Tests', function(){
     assert.equal(obj[3].arguments, undefined)
   });
 
-  test('Error de Sint&aacute;xis', function(){
+  test('Error de Sintaxis: ', function(){
     assert.throws(function() { pl0.parse("x = 323"); }, /Expected "."/);
   });
 
