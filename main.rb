74ce654a3897c61bd62d9b15a2bf274064e7a662
Cristo González Rodríguez <cristogr.93@gmail.com>
2014-03-27 09:46:53 +0000

    Intentando corregir base de datos

--- main.rb
+++ main.rb
@@ -55,12 +55,14 @@ post '/save' do
   else
                    
         if PL0Program.all.size > 9
-                 num = rand(1..9)
-                 c = PL0Program.get(num)
+                 #num = rand(1..9)
+                 #c = PL0Program.get(num)
+                 #c.destroy!
+                 c = PL0Program.all.sample()
                  c.destroy!
         end
    # if PL0Program.all.size < 10
-                 c = PL0Program.new
+    c = PL0Program.new
     c.name = params["fname"]
     c.source = params["input"]
     c.save
